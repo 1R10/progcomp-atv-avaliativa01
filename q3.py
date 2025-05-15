68e049954faf53976a55180798b7d3c9cd3f5ade
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-15 18:58:44 -0300

    Update q3.py

--- q3.py
+++ q3.py
@@ -7,4 +7,13 @@ if pergunta1 == nSorteado:
 else: 
     pergunta2 = int(input('Segunda tentativa: '))
     if pergunta2 == nSorteado:
-
+        print('Você acertou! O número era ', nSorteado)
+    else: 
+        pergunta3 = int(input('Terceira tentativa: '))
+        if pergunta3 == nSorteado:
+            print('Você acertou! O número era ', nSorteado)
+        else: 
+            pergunta4 = int(input('Ultima tentativa: '))
+            if pergunta4 == nSorteado:
+                print('Você acertou! O número era ', nSorteado)
+            else: print('Você errou. O número era ', nSorteado)
