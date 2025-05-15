d21f03dcaee3368259ab934769e8a1c3f9a0a610
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-15 20:23:38 -0300

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,4 @@
+di = int(input('Dia inicial: '))
+mi = int(input('Mês inicial: '))
+df = int(input('Dia final: '))
+mf = int(input('Mês final: '))
---
+++ q5.py
@@ -0,0 +1,3 @@
+minutos = 310 #float(input('Quantidade de minutos que o veículo ficou no estacionamento: '))
+horas   = minutos/60 
+valor = 0
