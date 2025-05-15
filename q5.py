b5d966d9bdd0ae27390dc534d2a0c5f224bea7f8
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-15 20:32:27 -0300

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,3 +1,9 @@
 minutos = 310 #float(input('Quantidade de minutos que o veículo ficou no estacionamento: '))
 horas   = minutos/60 
 valor = 0
+
+if horas >= 12:
+    print('O valor a ser pago é 30.')
+else:
+    if horas >= 2:
+        valor = horas * 8
