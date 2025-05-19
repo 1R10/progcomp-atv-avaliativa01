7089db7ea76e55ed77e582e15a1b9d618fbf9832
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-19 14:45:24 -0300

    Update q5.py
    Eu tinha esquecido de contar a fração como hora a mais

--- q5.py
+++ q5.py
@@ -1,9 +1,10 @@
-# Valor errado. Lembrar de corrigir
-
 minutos = int(input('Quantidade de minutos que o veículo ficou no estacionamento: '))
-horas   = minutos/60
-valor = 0
-print ('Como você ficou por ', horas, ' horas...')
+valor   = 0
+horas   = minutos // 60 # Transforma os minutos em horas
+fracao  = minutos  % 60
+
+if fracao > 0: # Vê se há uma fração dos minutos restantes e converte a mesma em +1 hora (se houver)
+      horas += 1 
 
 if horas > 12: # Se a hora for +12 ele nem precisa perder tempo com o resto do código
     print('O valor a ser pago é de R$30,00.')
@@ -11,10 +12,11 @@ if horas > 12: # Se a hora for +12 ele nem precisa perder tempo com o resto do c
 elif horas <= 2: # Até duas horas
         valor = horas * 8
 
-elif (horas >2 ) and (horas <= 4): # se for maior que duas horas ele já pula pra esse elif que soma o valor e reduz as 2 horas que precisam ser multiplicadas
-      valor = 16 + ((horas - 2) * 5)
+elif horas <= 4: # se for maior que duas horas ele já pula pra esse elif que soma o valor e reduz as 2 horas que precisam ser multiplicadas
+      valor = (horas - 2) * 5 + 16
 
-elif (horas > 4) and (horas <= 12): # Mesma coisa, mas entre 4h e 1 milésimo de segundo e 12h
-      valor = 16 + 10 + ((horas - 4) * 3)
+else:
+       # Mesma coisa, mas a partir 4h e 1 milésimo de segundo
+      valor = (horas - 4) * 3 + 16 + 10
 
-print('O valor a ser pago é de R$', round(valor, 2))
+print('O valor a ser pago é de R$', valor)
