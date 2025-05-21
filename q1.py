30835b083d798520fd88e1fde8ec440ac036d878
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-21 17:51:05 -0300

    Update q1.py
    Alterei erro. Antes ele estava rodando mesmo se o número tivesse mais que 4 dígitos.

--- q1.py
+++ q1.py
@@ -7,17 +7,17 @@ if nCompleto < 0:
 # O número é dividido de acordo com sua casa decimal 
 # (milhar, dezena, centena e unidade)
 # o nCompleto será reduzido até virar apenas a unidade
+elif  0>= nCompleto >= 9999:
+    milhar = nCompleto//1000
+    nCompleto = nCompleto - milhar*1000
 
-milhar = nCompleto//1000
-nCompleto = nCompleto - milhar*1000
+    centena = nCompleto//100
+    nCompleto = nCompleto - centena*100
 
-centena = nCompleto//100
-nCompleto = nCompleto - centena*100
+    dezena = nCompleto//10
+    nCompleto = nCompleto - dezena*10
 
-dezena = nCompleto//10
-nCompleto = nCompleto - dezena*10
+    unidade = nCompleto # Troquei o nome apenas para uma melhor leitura do código
 
-unidade = nCompleto # Troquei o nome apenas para uma melhor leitura do código
-
-soma = milhar + centena + dezena + unidade
-print('A soma de ', milhar, '+ ', centena, ' + ', dezena, ' + ',unidade, ' = ', soma )
+    soma = milhar + centena + dezena + unidade
+    print('A soma de ', milhar, '+ ', centena, ' + ', dezena, ' + ',unidade, ' = ', soma )
