ffc0b3f3150ad5d310a51a801d3b0e9722e730b3
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-22 17:10:43 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,23 +1,27 @@
-nCompleto = int(input('Digite um número de 4 dígitos entre 0 - 9999: '))
-if nCompleto >= 10000:
-    print('O número não pode estar acima de 9999.')
-if nCompleto < 0:
-    print('O número não pode estar abaixo de 0.')
+nCompleto = int(input('Digite um número de até 4 dígitos: '))
 
+if nCompleto >  9999:
+    print('O número não pode ser maior que 9999.')
+if nCompleto < -9999:
+    print('O número não pode ser menor que -9999')
+elif -9999 <= nCompleto <= 9999 :
 # O número é dividido de acordo com sua casa decimal 
 # (milhar, dezena, centena e unidade)
 # o nCompleto será reduzido até virar apenas a unidade
-elif  0>= nCompleto >= 9999:
-    milhar = nCompleto//1000
-    nCompleto = nCompleto - milhar*1000
 
-    centena = nCompleto//100
-    nCompleto = nCompleto - centena*100
+# Se o número for negativo, será transformado em positivo para a soma dos ALGARÍSMOS
 
-    dezena = nCompleto//10
-    nCompleto = nCompleto - dezena*10
+    if nCompleto < 0:
+        nCompleto = nCompleto *(-1)
+    milhar = nCompleto//1000                    # 1234//1000 = 1
+    nCompleto = nCompleto - milhar*1000         # 1234 - 1000 =  234
+    
+    centena = nCompleto//100                    # 234//100 = 2
+    nCompleto = nCompleto - centena*100         # 234 - 200 = 34
+    
+    dezena = nCompleto//10                      # 34//10 = 3
+    nCompleto = nCompleto - dezena*10           # 34 - 30 = 4
 
     unidade = nCompleto # Troquei o nome apenas para uma melhor leitura do código
 
-    soma = milhar + centena + dezena + unidade
-    print('A soma de ', milhar, '+ ', centena, ' + ', dezena, ' + ',unidade, ' = ', soma )
+    soma = milhar + centena + dezena + unidade  #soma = 1 + 2 + 3 + 4
