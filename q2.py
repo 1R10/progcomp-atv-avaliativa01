7dc7ec1bd04cc17e857ba2b1897f788634dc50d7
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-26 15:19:37 -0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,21 +1,21 @@
-# Coleta as notas dos 4 bimestres
+# Coleta as notas dos 4 bimestres e calcula a média
 bim1 = float(input('Insira a nota do 1° bimestre: '))
 bim2 = float(input('Insira a nota do 2° bimestre: '))
 bim3 = float(input('Insira a nota do 3° bimestre: '))
 bim4 = float(input('Insira a nota do 4° bimestre: '))
-mediaDisciplina = (2*bim1 + 2*bim2 + 3*bim3 + 3*bim4)/10 # Soma as 4 notas com seus pesos e divide por 10
+mediaDisciplina = (2*bim1 + 2*bim2 + 3*bim3 + 3*bim4) / 10
 
-if mediaDisciplina > 59:
+if mediaDisciplina >= 60:
     print('Parabéns! O aluno foi aprovado.')
 
-if mediaDisciplina <= 59: # Nota abaixo da média/60
-    if mediaDisciplina >= 20: # Critério de recuperação
+else: # Menor que 60
+    if mediaDisciplina >= 20: # Critério da recuperação
         notaRecup = float(input('Insira a nota da recuperação: '))
-        media_Final = (mediaDisciplina +  notaRecup)/2
+        media_Final = (mediaDisciplina + notaRecup) / 2
 
-        if media_Final > 60:
+        if media_Final >= 60:
             print('Parabéns! O aluno foi aprovado.')
-        else: print('O aluno foi reprovado')
-
-    if mediaDisciplina < 20:
+        else:
+            print('O aluno foi reprovado.')
+    else:
         print('O aluno reprovou e não tem direito à recuperação.')
